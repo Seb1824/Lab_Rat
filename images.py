4851fcced410824baa03a0c3bfcdb9ae9dbb0cbd
AnLu996 <coar.andreacuela@gmail.com>
2025-05-24 11:47:48 -0500

    Cambios para pantalla de inicio, perdiste y ganaste

--- images.py
+++ images.py
@@ -3,9 +3,19 @@
 import pygame
 from constants import CELL_SIZE
 
-def load_scaled_image(path):
+def load_scaled_image(path, target_size=None):
     image = pygame.image.load(path).convert_alpha()
-    return pygame.transform.scale(image, (CELL_SIZE, CELL_SIZE))
+    if target_size:
+        image = pygame.transform.scale(image, target_size)
+    return image
+
+
+# Fondo y botones
+start_bg = load_scaled_image("assets/start_bg.png", (600, 600))
+win_bg = load_scaled_image("assets/win_bg.png", (600, 600))
+button_play = load_scaled_image("assets/button_play.png", (200, 80))
+#button_retry = load_scaled_image("assets/button_retry.png", size=(200, 80))
+lose_overlay = load_scaled_image("assets/lose_overlay.png", (400, 200))
 
 # Carga las imágenes base
 rat_images = load_scaled_image("assets/rat.png")
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ pygame.init()
 WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Lab Rat Escape")
 
-from images import rat_images, robot_images, cheese_images
+from images import rat_images, robot_images, cheese_images, start_bg, win_bg, button_play, lose_overlay
 
 # Retardo en frames
 robot_move_delay = 2  
@@ -35,75 +35,115 @@ def draw_image(pos, image, offset=(0, 0)):
     y = pos[0] * CELL_SIZE + offset[1]
     WIN.blit(image, (x, y))
 
+def button_clicked(button_rect):
+    return button_rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]
+
+def start_screen():
+    while True:
+        WIN.blit(start_bg, (0, 0))
+        btn_rect = WIN.blit(button_play, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 40))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit(); exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button_clicked(btn_rect):
+                    return
+
+def end_screen(won):
+    while True:
+        if won:
+            WIN.blit(win_bg, (0, 0))
+        else:
+            draw_grid()
+            s = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+            s.set_alpha(180)
+            s.fill((0, 0, 0))
+            WIN.blit(s, (0, 0))
+            WIN.blit(lose_overlay, (SCREEN_WIDTH // 2 - 200, SCREEN_HEIGHT // 2 - 100))
+
+        btn_rect = WIN.blit(button_play, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 60))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit(); exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button_clicked(btn_rect):
+                    return
+
 
 def main():
     global player_pos, player_dir, robot_pos, robot_dir
-    clock = pygame.time.Clock()
-    running = True
-    global robot_move_counter
-    cheese_shake_timer = 0
-    cheese_shake_duration = 5      # duración de la sacudida 
-    cheese_shake_cooldown = FPS * 2
-    cheese_shaking = False
-    cheese_offset = (0, 0)
 
+    while True:
+        start_screen()
+        # Reset positions y laberinto
+        player_dir = (0, 1)
+        robot_dir = (0, -1)
+        maze = generate_maze(GRID_HEIGHT, GRID_WIDTH)
+        goal_pos = (GRID_HEIGHT - 2, GRID_WIDTH - 2)
 
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+        cheese_shake_timer = 0
+        cheese_shake_duration = 5
+        cheese_shake_cooldown = FPS * 3
+        cheese_shaking = False
+        cheese_offset = (0, 0)
 
-        # Moimiento del queso
-        cheese_shake_timer += 1
+        robot_move_delay = 5
+        robot_move_counter = 0
 
-        if cheese_shake_timer >= cheese_shake_cooldown:
-            cheese_shaking = True
-            cheese_shake_timer = 0
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit(); exit()
 
-        if cheese_shaking:
-            cheese_offset = (random.randint(-3, 3), random.randint(-3, 3))
-            cheese_shake_duration -= 1
-            if cheese_shake_duration <= 0:
-                cheese_shaking = False
-                cheese_shake_duration = 5
+            keys = pygame.key.get_pressed()
+            player_pos, player_dir = update_player_position(keys, player_pos, player_dir, maze)
+
+            # Movimiento robot más lento
+            robot_move_counter += 1
+            if robot_move_counter >= robot_move_delay:
+                path = a_star(robot_pos, player_pos, maze)
+                if path:
+                    new_robot_pos = path[0]
+                    robot_dir = (new_robot_pos[0] - robot_pos[0], new_robot_pos[1] - robot_pos[1])
+                    robot_pos = new_robot_pos
+                robot_move_counter = 0
+
+            # Sacudida del queso
+            cheese_shake_timer += 1
+            if cheese_shake_timer >= cheese_shake_cooldown:
+                cheese_shaking = True
+                cheese_shake_timer = 0
+
+            if cheese_shaking:
+                cheese_offset = (random.randint(-3, 3), random.randint(-3, 3))
+                cheese_shake_duration -= 1
+                if cheese_shake_duration <= 0:
+                    cheese_shaking = False
+                    cheese_shake_duration = 5
+                    cheese_offset = (0, 0)
+            else:
                 cheese_offset = (0, 0)
-        else:
-            cheese_offset = (0, 0)
 
+            WIN.fill(WHITE)
+            draw_grid()
+            draw_image(goal_pos, cheese_images, offset=cheese_offset)
+            draw_image(player_pos, rat_images.get(player_dir, rat_images[(0, 1)]))
+            draw_image(robot_pos, robot_images.get(robot_dir, robot_images[(0, -1)]))
+            pygame.display.update()
 
-        # Movimiento del jugador
-        keys = pygame.key.get_pressed()
-        player_pos, player_dir = update_player_position(keys, player_pos, player_dir, maze)
-
-        # Movimiento del robot con retardo
-        robot_move_counter += 1
-        if robot_move_counter >= robot_move_delay:
-            path = a_star(robot_pos, player_pos, maze)
-            if path:
-                new_robot_pos = path[0]
-                robot_dir = (new_robot_pos[0] - robot_pos[0], new_robot_pos[1] - robot_pos[1])
-                robot_pos = new_robot_pos
-            robot_move_counter = 0
-
-        # Dibujar todo
-        WIN.fill(WHITE)
-        draw_grid()
-        draw_image(goal_pos, cheese_images, offset=cheese_offset)
-        draw_image(player_pos, rat_images.get(player_dir, rat_images[(0, 1)]))
-        draw_image(robot_pos, robot_images.get(robot_dir, robot_images[(0, -1)]))
-
-        pygame.display.update()
-
-        # Condiciones de victoria o derrota
-        if player_pos == goal_pos:
-            print(MSG_WIN)
-            running = False
-        elif robot_pos == player_pos:
-            print(MSG_LOSE)
-            running = False
-
-    pygame.quit()
+            if player_pos == goal_pos:
+                end_screen(won=True)
+                break
+            elif robot_pos == player_pos:
+                end_screen(won=False)
+                break
 
 if __name__ == "__main__":
     main()
