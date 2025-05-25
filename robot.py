7530d50356d6a8b94362ad1f19c1fe2e56d78f4d
Seb1824 <145509950+Seb1824@users.noreply.github.com>
2025-05-24 20:05:02 -0500

    comentarios en robot.py

--- robot.py
+++ robot.py
@@ -5,9 +5,11 @@ from constants import GRID_HEIGHT
 robot_pos = (GRID_HEIGHT - 2, 1)
 robot_dir = (0, 1)
 
+# Actualiza la posición del robot siguiendo el siguiente paso en el camino
 def update_robot_position(path):
     global robot_pos, robot_dir
     if path:
-        next_pos = path[0]
+        next_pos = path[0]  # Toma el siguiente punto del camino
         robot_dir = (next_pos[0] - robot_pos[0], next_pos[1] - robot_pos[1])
+        # Actualiza la posición
         robot_pos = next_pos
